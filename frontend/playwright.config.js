3b4b4a764134b31220f0e54d874c40f1f54626a7
ply <ply@plydeMacBook-Air.local>
2026-08-25 10:35:59 +0800

    提高CI浏览器测试稳定性

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   timeout: 60_000,
+  retries: process.env.CI ? 1 : 0,
   expect: {
     timeout: 10_000
   },
